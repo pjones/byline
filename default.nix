e6d2a6653d5626b69f0a0961d8add64d5e0cbaeb
Peter Jones <pjones@devalot.com>
2019-09-13 08:22:11 -0700

    Update build system to use the latest nix-hs

--- default.nix
+++ default.nix
@@ -2,7 +2,14 @@
 }:
 
 let
-  byline = import ./nix/cabal2nix.nix { inherit pkgs; };
-  haskell = pkgs.haskellPackages;
+  nix-hs-src = fetchGit {
+    url = "https://code.devalot.com/open/nix-hs.git";
+    rev = "f0574f297a95afc3755c9e9620e582871d4d4f6f";
+  };
 
-in haskell.callPackage "${byline}" { }
+  nix-hs = import "${nix-hs-src}/default.nix" { inherit pkgs; };
+
+in nix-hs {
+  cabal = ./byline.cabal;
+  flags = [ "build-examples" ];
+}
