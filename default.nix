08111eafffa2ffbc07cbc5efeb126c4c3d0e93c1
Peter Jones <pjones@devalot.com>
2020-06-08 13:56:39 -0700

    Upgrade nix-hs

--- default.nix
+++ default.nix
@@ -1,14 +1,8 @@
-{ pkgs ? import <nixpkgs> { }
-}:
+{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { }
+, nix-hs ? import sources.nix-hs { inherit pkgs; }, ghc ? "default" }:
 
-let
-  nix-hs = import (fetchGit {
-    url = "https://code.devalot.com/open/nix-hs.git";
-    rev = "136d1a5c1e87c2ef5e8050c6b521f4d529645eba";
-    ref = "next";
-  }) { inherit pkgs; };
-
-in nix-hs {
+nix-hs {
   cabal = ./byline.cabal;
   flags = [ "build-examples" ];
+  compiler = ghc;
 }
