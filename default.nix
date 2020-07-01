2fc01b8bb64d76e559c549f513a2d1c035711b87
Peter Jones <pjones@devalot.com>
2020-06-30 19:03:38 -0700

    Update symbolic links during a nix-build

--- default.nix
+++ default.nix
@@ -3,8 +3,31 @@
 , nix-hs ? import sources.nix-hs { inherit pkgs; }
 , ghc ? "default"
 }:
+let
+  # Files shared by all packags:
+  linked = {
+    "CHANGES.md" = ./CHANGES.md;
+    "LICENSE" = ./LICENSE;
+    "README.md" = ./README.md;
+    "Setup.hs" = ./Setup.hs;
+  };
 
+  # Patch each package by replacing symbolic links with copies of the
+  # actual files they link to.  This is necessary since Nix copies
+  # each package to the nix store separately, thus breaking the links.
+  postPatch = with pkgs.lib;
+    concatMapStringsSep "\n"
+      (file: ''
+        if [ -L ${file} ]; then
+          rm ${file}
+          cp ${linked.${file}} ${file}
+        fi
+      '')
+      (attrNames linked);
+in
 nix-hs {
+  inherit postPatch;
+
   cabal = {
     byline = ./byline/byline.cabal;
   };
