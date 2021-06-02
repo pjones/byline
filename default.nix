dcdb46a0dd36e4a52d5c2af05d548c8327736559
Peter Jones <pjones@devalot.com>
2021-06-02 11:50:33 -0700

    Update dependencies so the package can compile w/ GHC 9.0.1

--- default.nix
+++ default.nix
@@ -6,24 +6,11 @@
 
 nix-hs {
   cabal = ./byline.cabal;
-
   flags = [ "build-examples" ];
   compiler = ghc;
 
   overrides = lib: self: super: {
-    haskeline =
-      if super ? haskeline_0_8_1_0
-      then lib.dontCheck super.haskeline_0_8_1_0
-      else super.haskeline;
-
-    ansi-terminal =
-      if super ? ansi-terminal_0_11
-      then super.ansi-terminal_0_11
-      else super.ansi-terminal;
-
-    optparse-applicative =
-      if super ? optparse-applicative_0_16_0_0
-      then super.optparse-applicative_0_16_0_0
-      else super.optparse-applicative;
+    attoparsec = super.attoparsec_0_14_1;
+    relude = super.relude_1_0_0_1;
   };
 }
